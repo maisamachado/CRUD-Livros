af95979213e7ead0cee9ce6bb558a22710dbb301
maisamachado <97968320+maisamachado@users.noreply.github.com>
2022-06-17 11:07:43 -0300

    Update Principal.java

--- src/project/Principal.java
+++ src/project/Principal.java
@@ -5,7 +5,7 @@ import project.controller.MenuController;
 public class Principal {
     public static void main(String[] args) {
         MenuController menu = new MenuController();
-        System.out.println("#### Boas vindas a unilove Library! ####");
+        System.out.println("#### Boas vindas a UNIFACS Library! ####");
         System.out.print("\n========== Escolha uma opção ==========\n");
         menu.menu();
     }
